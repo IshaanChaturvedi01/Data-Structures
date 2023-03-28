41b15422905ab5fde74ecfb8ce1cc9e6a08f7157
Ishaan Chaturvedi <125291959+IshaanChaturvedi01@users.noreply.github.com>
2023-03-28 22:15:01 +0530

    Update Factorial of a number.cpp

--- Factorial of a number.cpp
+++ Factorial of a number.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {
     int n,res=1;
+    cout<<"Enter any number:";
+    cin>>n;
     for(int i=2;i<=n;i++)
     {
         res=res*i;
-        return res;
-        
     }
+        return res;
 }
